f8d577e7b3a8c129d6b4d3d4fa329e69e6ad0663
AkashNadar <Akashnadar14@gmail.com>
2022-08-01 22:16:55 +0530

    created weather app using weather api and tailwind

---
+++ index.js
@@ -0,0 +1,42 @@
+// OpenWeather Api
+
+var loc = document.getElementById("loc");
+var img = document.getElementById("weathImg")
+var temp = document.getElementById("temp");
+var desc = document.getElementById("desc");
+var humid = document.getElementById("humid");
+var wind = document.getElementById("wind");
+var imgSrc = document.getElementById("weathImg");
+
+var weatherApi = (location) => {
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=7f97da1820f236e1987e1cadcfa9cc1c&units=metric`)
+    .then((res) => res.json())
+    .then((data) => {
+
+        // console.log(data);
+        loc.innerHTML = `Weather in ${data.name}`;
+        temp.innerHTML = `${data.main.temp}°C`;
+        desc.innerHTML = `${data.weather[0].description}`;
+        humid.innerHTML = `${data.main.humidity}%`;
+        wind.innerHTML = `${data.wind.speed}km/h`;
+        imgSrc.src = `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
+    })
+    .catch((err) => {console.log(`Some Error Found:- ${err}`);
+    });
+} 
+
+weatherApi("Mumbai");
+
+
+var getWeather = document.getElementById("getWeather");
+
+getWeather.onclick = (inputLoc) => {
+    
+    console.log(inputLoc);
+};
+getWeather.onclick = (inputLoc) => {
+    var inputLoc = document.getElementById("inputLoc").value;
+    if(inputLoc.length <= 0) alert("Location cannot be Empty");
+    else
+        weatherApi(inputLoc);
+};
